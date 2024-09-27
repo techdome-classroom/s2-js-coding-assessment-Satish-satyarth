0793776e1f9a91f084647c648debe430a6fbdb6a
satish-satyarth <satishsatyarth2002@gmail.com>
2024-09-27 13:57:00 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -3,35 +3,31 @@
  * @return {number}
  */
 var romanToInt = function(s) {
-    
-    function romanToInt(s) {
-        // Dictionary for Roman numeral values
-        const romanDict = {
-            'I': 1,
-            'V': 5,
-            'X': 10,
-            'L': 50,
-            'C': 100,
-            'D': 500,
-            'M': 1000
-        };
-    
-        let total = 0;
-    
-        // Loop through the Roman numeral string
-        for (let i = 0; i < s.length; i++) {
-            // If the current Roman numeral is smaller than the next one, subtract it
-            if (i + 1 < s.length && romanDict[s[i]] < romanDict[s[i + 1]]) {
-                total -= romanDict[s[i]];
-            } else {
-                // Otherwise, add its value to the total
-                total += romanDict[s[i]];
-            }
+    // Dictionary for Roman numeral values
+    const romanDict = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    };
+
+    let total = 0;
+
+    // Loop through the Roman numeral string
+    for (let i = 0; i < s.length; i++) {
+        // If the current Roman numeral is smaller than the next one, subtract it
+        if (i + 1 < s.length && romanDict[s[i]] < romanDict[s[i + 1]]) {
+            total -= romanDict[s[i]];
+        } else {
+            // Otherwise, add its value to the total
+            total += romanDict[s[i]];
         }
-    
-        return total;
     }
-    
+
+    return total;
 };
 
-module.exports={romanToInt}
+module.exports = { romanToInt };
