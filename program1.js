53ea641e30b9ba850184f93b4da1e05cc41c25f2
satish-satyarth <satishsatyarth2002@gmail.com>
2024-09-27 13:43:14 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -4,8 +4,40 @@
  */
 var isValid = function(s) {
     
-
+    function isValidParentheses(str) {
+        const stack = [];
+        const pairs = {
+            "(": ")",
+            "[": "]",
+            "{": "}",
+        };
     
+        for (let char of str) {
+            if (pairs[char]) {
+                stack.push(char);
+            } else if (
+                char === ")" ||
+                char === "]" ||
+                char === "}"
+            ) {
+                if (
+                    pairs[stack.pop()] !==
+                    char
+                ) {
+                    return false;
+                }
+            }
+        }
+    
+        return stack.length === 0;
+    }
+    
+    const inputString = "({()})";
+    console.log(
+        `Is it a valid Paranthesis ? :
+    ${isValidParentheses(inputString)}`
+    );
+
 
 };
 
