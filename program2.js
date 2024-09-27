9b33d2b7504a106ab8a869e599d6c5c01a300c37
satish-satyarth <satishsatyarth2002@gmail.com>
2024-09-27 13:57:18 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -16,7 +16,7 @@ var romanToInt = function(s) {
 
     let total = 0;
     for (let i = 0; i < s.length; i++) {
-        // If the current Roman numeral is smaller than the next one, subtract it
+        
         if (i + 1 < s.length && romanDict[s[i]] < romanDict[s[i + 1]]) {
             total -= romanDict[s[i]];
         } else {
