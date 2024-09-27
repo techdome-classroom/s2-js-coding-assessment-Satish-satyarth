f1f2adc4bb0825ab3378438ebea2f1069aa16355
satish-satyarth <satishsatyarth2002@gmail.com>
2024-09-27 13:46:36 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -2,42 +2,35 @@
  * @param {string} s
  * @return {number}
  */
-var romanToInt = function(s) {
-    
-    function romanToDecimal(str) {
-        const romanMap = {
-            'I': 1,
-            'V': 5,
-            'X': 10,
-            'L': 50,
-            'C': 100,
-            'D': 500,
-            'M': 1000
-        };
-    
-        let sum = 0;
-        for (let i = 0; i < str.length; i++) {
-        
-            // If the current value is less than the next value,
-            // subtract current from next and add to sum
-            if (i + 1 < str.length && romanMap[str[i]] < romanMap[str[i + 1]]) {
-                sum += romanMap[str[i + 1]] - romanMap[str[i]];
-                
-                // Skip the next symbol
-                i++;
-            } else {
-            
-                
-                sum += romanMap[str[i]];
-            }
-        }
-    
-        return sum;
-    }
-    
-    console.log(romanToDecimal("IX")); // Output: 9
+var romanToInt = function (s) {
+  function romanToDecimal(str) {
+    const romanMap = {
+      I: 1,
+      V: 5,
+      X: 10,
+      L: 50,
+      C: 100,
+      D: 500,
+      M: 1000,
+    };
 
+    let sum = 0;
+    for (let i = 0; i < str.length; i++) {
+      // If the current value is less than the next value,
+      // subtract current from next and add to sum
+      if (i + 1 < str.length && romanMap[str[i]] < romanMap[str[i + 1]]) {
+        sum += romanMap[str[i + 1]] - romanMap[str[i]];
+
+        i++;
+      } else {
+        sum += romanMap[str[i]];
+      }
+    }
+
+    return sum;
+  }
+
+  console.log(romanToDecimal("IX")); // Output: 9
 };
 
-
-module.exports={romanToInt}
+module.exports = { romanToInt };
