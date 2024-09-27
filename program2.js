f462e45a1a74ece9d413f0d7086d358bc1129710
satish-satyarth <satishsatyarth2002@gmail.com>
2024-09-27 13:55:31 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -2,34 +2,11 @@
  * @param {string} s
  * @return {number}
  */
-var romanToInt = function (s) {
-  function romanToDecimal(str) {
-    const romanMap = {
-      I: 1,
-      V: 5,
-      X: 10,
-      L: 50,
-      C: 100,
-      D: 500,
-      M: 1000,
-    };
+var romanToInt = function(s) {
+    
 
-    let sum = 0;
-    for (let i = 0; i < str.length; i++) {
-
-      if (i + 1 < str.length && romanMap[str[i]] < romanMap[str[i + 1]]) {
-        sum += romanMap[str[i + 1]] - romanMap[str[i]];
-
-        i++;
-      } else {
-        sum += romanMap[str[i]];
-      }
-    }
-
-    return sum;
-  }
-
-  console.log(romanToDecimal("IX"));
+    
 };
 
-module.exports = { romanToInt };
+
+module.exports={romanToInt}
